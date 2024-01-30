43288681956a2637aec3947ffeac05b80e37cdec
mogaogelwa <157511374+mogaogelwa@users.noreply.github.com>
2024-01-30 10:36:05 +0200

    Add files via upload

---
+++ css2024_day1/untitled0.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 29 12:15:17 2024
+
+@author: Joshu
+"""
+'''
+import pandas as pd 
+
+file = pd.read_csv("country_data.csv")
+print (file)
+
+print(file.info())
+print(file.describe())
+
+
+import pandas as pd
+file = pd.read_csv("iris.csv")
+print(file)
+
+print(file.info())
+
+'''
+'''
+import pandas as pd
+file = pd.read_csv("insurance_data.csv", skiprows=5)
+
+print(file)
+'''
+import pandas as pd
+file = pd.read_csv("insurance_data.csv", skiprows=4)
+
+file
+
+
+
+
+
